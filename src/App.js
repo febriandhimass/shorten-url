1999c47a5c61730241bb5eb9e73ab1b16c9289a2
febriands <febrian.dhimassyahfitra@ai.astra.co.id>
2020-01-03 15:21:33 +0700

    using shorten

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Shorten from './components/Shorten'
+import Shortener from "@studiohyperdrive/shortener"
 
+const shortener = new Shortener({
+  target: "https://febrian.shorten",
+  length: 8,
+  alphabet: "alphanumeric"
+})
 function App() {
-  const [shortUrl, setShortUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState({})
 
   const shorten = searchValue => {
-    fetch(`https://ismaelc-bitly.p.rapidapi.com/v3/shorten?longUrl=${searchValue}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "ismaelc-bitly.p.rapidapi.com",
-        "x-rapidapi-key": "SIGN-UP-FOR-KEY"
-      }
-    })
-    .then(response => {
-      console.log(response);
-      setShortUrl(response.data.url)
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    const result = shortener.shorten(searchValue)
+    setShortUrl(result)
   }
 
   return (
     <div className="App">
       <Header text="Shorten your url here" />
       <Shorten shorten={shorten} />
-      <p className="App-intro">Here it is &nbsp;<a href={shortUrl}>{shortUrl}</a></p>
+      <p className="App-intro">Here it is &nbsp;<a href={shortUrl.original} target="_blank" rel="noopener noreferrer">{shortUrl.target}</a></p>
     </div>
   );
 }
